d542ccbc52d07931bf1fcf47e0b6bdc3fe970957
Ben Quartermaine <bq@Bens-MacBook-Pro.local>
2019-08-01 17:34:37 +1000

    added underlined hero component

--- BQportfolio/src/components/header.js
+++ BQportfolio/src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import styled from 'styled-components'
-import { HeaderText, colors } from '../utilities'
+import { HeaderText, colors, media } from '../utilities'
 import BurgerMenu from './BurgerMenu'
 
 const HeaderWrapper = styled.header`
@@ -13,9 +13,10 @@ const HeaderWrapper = styled.header`
   padding: 0em 2em;
   color: ${colors.almostWhite};
   background: ${colors.black};
+
 `;
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <HeaderWrapper>
     <HeaderText>Ben Quartermaine</HeaderText>
     <BurgerMenu />
--- BQportfolio/src/pages/about.js
+++ BQportfolio/src/pages/about.js
@@ -1,13 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import { HeroTextUnderlined } from '../utilities'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const About = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <HeroTextUnderlined>About</HeroTextUnderlined>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
