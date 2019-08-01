1d896617f08e617219d26da10cdbe283e9afca94
Ben Quartermaine <bq@Bens-MacBook-Pro.local>
2019-08-01 18:44:09 +1000

    ..

--- BQportfolio/src/pages/about.js
+++ BQportfolio/src/pages/about.js
@@ -1,14 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
-import { HeroTextUnderlined } from '../utilities'
+import { HeroTextUnderlined, BodyText } from '../utilities'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from 'styled-components'
+
+const SectionWrapper = styled.section`
+	
+	
+`
+
+const ContentWrapper = styled.div`
+	max-width: 600px;
+`
+
 
 const About = () => (
   <Layout>
-    <SEO title="Page two" />
-    <HeroTextUnderlined>About</HeroTextUnderlined>
-    <p>Welcome to page 2</p>
+    <SEO title="About Page" />
+    <SectionWrapper>
+    	<HeroTextUnderlined>About</HeroTextUnderlined>
+    	<ContentWrapper>
+    		<BodyText>Welcome to page 2 and how it is we're going to Welcome to page 2 and how it is we're going toWelcome to page 2 and how it is we're going toWelcome to page 2 and how it is we're going toWelcome to page 2 and how it is we're going toWelcome to page 2 and how it is we're going toWelcome to page 2 and how it is we're going to</BodyText>
+    	</ContentWrapper>
+    </SectionWrapper>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
