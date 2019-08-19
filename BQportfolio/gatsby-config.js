c8820ad6a6eeeb717ecf744f33d5ebf84db23876
Ben Quartermaine <bq@Bens-MacBook-Pro.local>
2019-08-19 23:40:42 +1000

    favicon

--- BQportfolio/gatsby-config.js
+++ BQportfolio/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `BQ UI/UX`,
-    description: `Ben Quartermaine's Portfolio`,
+    title: `Ben Quartermaine | UX Designer`,
+    description: `Ben Quartermaine's Portfolio | UX Designer | Melbourne, Australia`,
     author: `@benjaminquartermaine`,
   },
   plugins: [
@@ -32,7 +32,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/bq-favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
