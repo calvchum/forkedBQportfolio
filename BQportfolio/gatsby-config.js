52f9ff5a018fc75bc3747cac4d1d018e66e12413
Calvin Cheung <calvchum@gmail.com>
2019-08-20 16:45:15 +0800

    added styled components to the config file

--- BQportfolio/gatsby-config.js
+++ BQportfolio/gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-transformer-remark`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
